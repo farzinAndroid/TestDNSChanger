67179a9ef983435850885acd5e3e120596cc7d2a
Farzin <farzinallahyari1234@gmail.com>
2024-08-10 09:51:34 +0330

    ConnectivityBackgroundService

--- app/src/main/java/com/farzin/testdnschanger/service/ConnectivityBackgroundService.kt
+++ app/src/main/java/com/farzin/testdnschanger/service/ConnectivityBackgroundService.kt
@@ -1,73 +1,64 @@
-package com.farzin.testdnschanger.service;
+package com.farzin.testdnschanger.service
 
-import android.app.Service;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.VpnService;
-import android.os.Build;
-import android.os.IBinder;
+import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.VpnService
+import android.os.Build
+import android.os.IBinder
+import com.farzin.testdnschanger.service.BackgroundVpnConfigureActivity.Companion.startBackgroundConfigure
 
-import androidx.annotation.Nullable;
-
-
-public class ConnectivityBackgroundService extends Service {
-    private BroadcastReceiver connectivityChange = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            boolean connected = !intent.hasExtra("noConnectivity");
-            int type = intent.getIntExtra("networkType", -1);
-            if(!connected)return;
-            if(type == ConnectivityManager.TYPE_WIFI){
-                startService();
-            }else if(type == ConnectivityManager.TYPE_MOBILE ){
-                startService();
+class ConnectivityBackgroundService : Service() {
+    private val connectivityChange: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val connected = !intent.hasExtra("noConnectivity")
+            val type = intent.getIntExtra("networkType", -1)
+            if (!connected) return
+            if (type == ConnectivityManager.TYPE_WIFI) {
+                startService()
+            } else if (type == ConnectivityManager.TYPE_MOBILE) {
+                startService()
             }
         }
-    };
+    }
 
-    private void startService(){
-        Intent i = VpnService.prepare(this);
+    private fun startService() {
+        val i = VpnService.prepare(this)
         if (i == null) {
-            Intent serviceIntent = new Intent(this, DNSVpnService.class);
-            serviceIntent.putExtra("start_vpn", true);
+            val serviceIntent = Intent(this, DNSVpnService::class.java)
+            serviceIntent.putExtra("start_vpn", true)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(serviceIntent);
+                startForegroundService(serviceIntent)
             } else {
-                startService(serviceIntent);
+                startService(serviceIntent)
             }
         } else {
-            BackgroundVpnConfigureActivity.startBackgroundConfigure(this, true);
+            startBackgroundConfigure(this, true)
         }
     }
 
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        registerReceiver(connectivityChange, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
-        ConnectivityManager cm =(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+    override fun onCreate() {
+        super.onCreate()
+        registerReceiver(connectivityChange, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+        val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        if(activeNetwork == null)return;
-        if(activeNetwork.getType() == ConnectivityManager.TYPE_WIFI ){
-            startService();
-        }else if(activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE ){
-            startService();
+        val activeNetwork = cm.activeNetworkInfo ?: return
+        if (activeNetwork.type == ConnectivityManager.TYPE_WIFI) {
+            startService()
+        } else if (activeNetwork.type == ConnectivityManager.TYPE_MOBILE) {
+            startService()
         }
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return START_STICKY;
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        return START_STICKY
     }
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    override fun onBind(intent: Intent): IBinder? {
+        return null
     }
 }
