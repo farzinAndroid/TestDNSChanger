9572dd34a72cc5e46863b7d22ed15e7f31d8864d
Farzin <farzinallahyari1234@gmail.com>
2024-08-10 09:58:12 +0330

    DNSChangerLight

--- app/src/main/java/com/farzin/testdnschanger/service/DNSChangerLight.kt
+++ app/src/main/java/com/farzin/testdnschanger/service/DNSChangerLight.kt
@@ -1,19 +1,13 @@
-package com.farzin.testdnschanger.service;
+package com.farzin.testdnschanger.service
 
-import android.app.Application;
+import android.app.Application
 
-
-public class DNSChangerLight extends Application {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-            @Override
-            public void uncaughtException(Thread t, Throwable e) {
-                System.out.println(e);
-//                ErrorDialogActivity.show(DNSChangerLight.this, e);
-            }
-        });
+class DNSChangerLight : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Thread.setDefaultUncaughtExceptionHandler { t, e ->
+            println(e)
+            //                ErrorDialogActivity.show(DNSChangerLight.this, e);
+        }
     }
 }
